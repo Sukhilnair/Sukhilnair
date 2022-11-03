2960de7d55d2430b4666395a03f1d08d500e4cdc
Sukhilnair <102207536+Sukhilnair@users.noreply.github.com>
2022-11-03 16:21:45 +0530

    Update USB_display.py

--- USB_display.py
+++ USB_display.py
@@ -9,21 +9,27 @@ import time
 import requests
 import threading
 import socket
+print ("##########################################################inside client format####################################################")
 def event_json_based_hw_control(event_json, enable_relay_flag, enable_gpio_flag, signal_usb_relay_method, signal_gpio_method):
     try:
-        for tmp in event_json['event']['tags']:
-            if tmp['key'] == 'lp':
-              ser=serial.Serial("/dev/ttyUSB0",9600)
-              if not ser.isOpen():
-              ser.open()
-              print('Serial port is open',ser.isOpen())
-              inputString = tmp['value']
-              outputString = inputString.encode('utf-8').hex()
-              reuslt = r'\x'.join(outputString[i:i+2] for i in range(0, len(outputString), 2))
-              def ts(str):
-                 command = b"\x30\x30\x32\x44\x49\x53\x3D\x{}\x0D\x0A"
-                 ser.write(command)
-                 print(command)
+        value = event_json['event']['name']
+        ser=serial.Serial("/dev/ttyUSB0",9600)
+        ser.close()
+        ser.open()
+        print('Serial port is open',ser.isOpen())
+        inputString = "002DIS="+value[0:8]
+        print(inputString)
+        outputString = inputString.encode('utf-8').hex()
+        print(outputString)
+        reuslt = r"\x"+r'\x'.join(outputString[i:i+2] for i in range(0, len(outputString), 2))
+        print (reuslt)
+        command = "b"+'"'+reuslt+'"'
+        sent = reuslt.encode()
+        print (sent)
+        rece = (reuslt.encode()).decode('unicode-escape')
+        rece += "\r\n"
+        print (rece.encode())
+        ser.write(rece.encode())
     except Exception as err:
         print("Error while triggering relay : ", err)
-    return 
+    return
